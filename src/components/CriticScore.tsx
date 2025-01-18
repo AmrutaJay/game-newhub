52c91493f40dcf4869c64d2ecd3d74d04d6ed89d
Shalan <shalanpatil20598@gmail.com>
2025-01-18 20:42:42 +0530

    displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,19 @@
+import { Badge, HStack } from "@chakra-ui/react";
+
+interface Props {
+    score:number;
+}
+
+const CriticScore = ({score} : Props) => {
+  let color = score >90 ?'green' : score >80 ? 'yellow' : '';
+
+    return (
+    
+
+<Badge colorScheme={color} fontSize='14px' paddingX={2} borderRadius={4}>{score}</Badge>
+  
+    
+  )
+}
+
+export default CriticScore
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,9 @@
 
 import { Game } from '../hooks/useGames';
-import { CardBody, Image,Card, Heading } from '@chakra-ui/react';
-
+import { Text, CardBody, Image,Card, Heading, HStack, space } from '@chakra-ui/react';
+import { Platform } from '../hooks/useGames';
+import PlatformIconList from './PlatformIconList';
+import CriticScore from './CriticScore';
 
 interface Props {
     game:Game
@@ -14,6 +16,12 @@ const GameCard = ({game} : Props) => {
         <Image src= {game.background_image} />
 <CardBody>
     <Heading fontSize='2xl'>{game.name}</Heading>
+    <HStack justifyContent='space-between '>
+
+<PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+  <CriticScore score={game.metacritic} />
+      
+    </HStack>
 </CardBody>
 
     </Card>
