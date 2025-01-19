a353ea1a1ed3f481911787774500070f0b44eb9a
Shalan <shalanpatil20598@gmail.com>
2025-01-19 23:56:03 +0530

    Refractor:Remove dublicated style

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,7 +3,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
 
 const GameCardSkeleton = () => {
   return (
-    <Card width='300px' borderRadius={10} overflow='hidden'>
+    <Card>
         
         <Skeleton startColor='pink.500' endColor='orange.500' height="200px" />
 <CardBody>
