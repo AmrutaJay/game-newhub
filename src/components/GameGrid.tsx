a451ea9aa72402be17e0d9d6e1f53e868ad1c88d
Shalan <shalanpatil20598@gmail.com>
2025-01-28 16:08:19 +0530

    clean up the game card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -19,7 +19,7 @@ const Skeletons = [1,2,3,4,5,6]
     <>
     {error && <Text>{error}</Text>}
     <SimpleGrid 
-    columns={{sm:1, md:2, lg:3, xl:5}} padding='10px' spacing={3}>
+    columns={{sm:1, md:2, lg:3, xl:4}} padding='10px' spacing={6}>
         {isLoading && Skeletons.map(skeleton => <GameCardContainer key={skeleton}> <GameCardSkeleton /> </GameCardContainer>)}
         
         {data.map((game) => (<GameCardContainer key={game.id} >  <GameCard  game={game}/> </GameCardContainer>))}
